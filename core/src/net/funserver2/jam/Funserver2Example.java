9475e8ea12ad0068aacdd43b4007680dfd7b219c
MartensCedric <cedricmartens98@gmail.com>
2016-05-08 17:17:40 -0400

    Music should loop now

--- core/src/net/funserver2/jam/Funserver2Example.java
+++ core/src/net/funserver2/jam/Funserver2Example.java
@@ -5,19 +5,30 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public class Funserver2Example extends ApplicationAdapter
 {
 	private Texture dropImage;
 	private Texture bucketImage;
+
 	private Sound dropSound;
 	private Music rainMusic;
+
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+
+	private Rectangle bucket;
 	
 	@Override
 	public void create()
 	{
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+
 		dropImage = new Texture("droplet.png");
 		bucketImage = new Texture("bucket.png");
 
@@ -26,11 +37,26 @@ public class Funserver2Example extends ApplicationAdapter
 
 		rainMusic.setLooping(true);
 		rainMusic.play();
+
+		batch = new SpriteBatch();
+
+		bucket = new Rectangle();
+		bucket.x = 800 / 2 - 64 / 2;
+		bucket.y = 20;
+
+		bucket.width = 64;
+		bucket.height = 64;
 	}
 
 	@Override
 	public void render()
 	{
-
+		Gdx.gl.glClearColor(0,0,0.2f,1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		batch.draw(bucketImage, bucket.x, bucket.y);
+		batch.end();
 	}
 }
