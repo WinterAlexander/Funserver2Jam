d4e2f212de5959586371e38a7c5aa65fdbea89d3
Alexander Winter <a.w1nter@hotmail.com>
2016-05-08 17:36:45 -0400

    Finished bucket movements, better than tutorial

--- core/src/net/funserver2/jam/Funserver2Example.java
+++ core/src/net/funserver2/jam/Funserver2Example.java
@@ -2,6 +2,7 @@ package net.funserver2.jam;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
@@ -9,6 +10,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 
 public class Funserver2Example extends ApplicationAdapter
 {
@@ -22,12 +24,15 @@ public class Funserver2Example extends ApplicationAdapter
 	private SpriteBatch batch;
 
 	private Rectangle bucket;
+	private Vector3 touchPos;
 	
 	@Override
 	public void create()
 	{
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 800, 480);
+
+		touchPos = new Vector3();
 
 		dropImage = new Texture("droplet.png");
 		bucketImage = new Texture("bucket.png");
@@ -51,12 +56,31 @@ public class Funserver2Example extends ApplicationAdapter
 	@Override
 	public void render()
 	{
-		Gdx.gl.glClearColor(0,0,0.2f,1);
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		camera.update();
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		batch.draw(bucketImage, bucket.x, bucket.y);
 		batch.end();
+
+		if(Gdx.input.isTouched())
+		{
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPos);
+			bucket.x = touchPos.x - 64 / 2;
+		}
+
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
+			bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+			bucket.x += 200 * Gdx.graphics.getDeltaTime();
+
+		if(bucket.x < -bucket.width / 2)
+			bucket.x = -bucket.width / 2;
+
+		if(bucket.x > camera.viewportWidth - bucket.width / 2)
+			bucket.x = camera.viewportWidth - bucket.width / 2;
 	}
 }
