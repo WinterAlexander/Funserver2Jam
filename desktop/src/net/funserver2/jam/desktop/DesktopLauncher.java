8b3874c193a005643a7da67f6b66518db0997720
MartensCedric <cedricmartens98@gmail.com>
2016-05-08 16:20:47 -0400

    Added title and did a perfect frame size

--- desktop/src/net/funserver2/jam/desktop/DesktopLauncher.java
+++ desktop/src/net/funserver2/jam/desktop/DesktopLauncher.java
@@ -9,6 +9,12 @@ public class DesktopLauncher
 	public static void main(String[] arg)
 	{
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title = "Fun server2";
+		config.width = 1280;
+		config.height = 720;
+
 		new LwjglApplication(new Funserver2Example(), config);
+
 	}
 }
